4e31015b3b16f6a80e7fc9fb0ff4730f660108d7
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2023-09-15 14:32:44 +0530

    Setup design system

---
+++ src/pages/_document.jsx
@@ -0,0 +1,15 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
+  theme: {
+    fontFamily: {
+      outfit: "var(--ff-outfit)",
+    },
+    fontWeight: {
+      normal: 300,
+      bold: 500,
+    },
+    fontSize: {
+      sm: "var(--fs-sm)",
+      base: "var(--fs-base)",
+      md: "var(--fs-md)",
+      lg: "var(--fs-lg)",
+      xl: "var(--fs-xl)",
+    },
+    colors: {
+      highlight: "var(--highlight)",
+      primary: "var(--primary)",
+      background: "var(--background)",
+      red: {
+        900: "var(--red-900)",
+      },
+      blue: {
+        300: "var(--blue-300)",
+        700: "var(--blue-700)",
+        900: "var(--blue-900)",
+      },
+      gray: {
+        100: "var(--gray-100)",
+      },
+    },
+    opacity: {
+      75: "0.75",
+      100: "1",
+    },
+    letterSpacing: {
+      tight: "-0.03125em", // -0.5px
+    },
+  },
   plugins: [],
 };
