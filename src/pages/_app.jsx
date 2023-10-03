1a7004dc6a8d9909fbf7690b8287c218f43871e6
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2023-10-03 11:22:54 +0530

    Fix footer position

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -11,14 +11,16 @@ export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   return (
-    <div className="mx-auto grid max-w-[90rem] grid-cols-1 grid-rows-[auto,auto] justify-center lg:grid-cols-[auto,minmax(0,1fr)] lg:grid-rows-[auto] lg:gap-x-9 lg:p-8">
-      <Header />
-      <BookmarksProvider>
-        <SearchProvider key={router.pathname}>
-          <Component {...pageProps} />
-        </SearchProvider>
-      </BookmarksProvider>
+    <>
+      <div className="mx-auto grid max-w-[90rem] grid-cols-1 grid-rows-[auto,auto] justify-center lg:grid-cols-[auto,minmax(0,1fr)] lg:grid-rows-[auto] lg:gap-x-9 lg:p-8">
+        <Header />
+        <BookmarksProvider>
+          <SearchProvider key={router.pathname}>
+            <Component {...pageProps} />
+          </SearchProvider>
+        </BookmarksProvider>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
