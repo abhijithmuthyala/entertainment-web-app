264b92b1478f26789bd85876946f92215989c92e
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2024-07-04 18:06:57 +0530

    Implement infinite scrolling for search results

---
+++ src/components/ObserveIntersection.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useMemo, useRef } from "react";
+
+const defaultOptions = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 1.0,
+};
+
+export default function ObserveIntersection({
+  onIntersection,
+  options = {},
+  className = "",
+  children,
+}) {
+  const ref = useRef(null);
+  const mergedOptionsMemo = useMemo(
+    function memoiseOptions() {
+      return Object.assign({ ...defaultOptions }, options);
+    },
+    [options],
+  );
+  useIntersectionObserver(ref, mergedOptionsMemo, onIntersection);
+
+  return (
+    <div className="relative">
+      {children}
+      <div
+        ref={ref}
+        className={`absolute bottom-0 h-8 w-full ${className}`}
+      ></div>
+      ;
+    </div>
+  );
+}
+
+function useIntersectionObserver(nodeRef, options, onIntersection) {
+  useEffect(
+    function observeIntersectionEffect() {
+      const node = nodeRef.current;
+      const observer = new IntersectionObserver(observerCallback, options);
+      observer.observe(node);
+
+      function observerCallback([entry]) {
+        if (!entry.isIntersecting) return;
+        onIntersection(entry);
+      }
+
+      return function unobserveIntersection() {
+        observer.unobserve(node);
+      };
+    },
+    [options, nodeRef, onIntersection],
+  );
+}
--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,19 +1,32 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+function paginationReducer(page, action) {
+  switch (action.type) {
+    case "next":
+      return page + action.step ?? 1;
+    case "previous":
+      return page - action.step ?? 1;
+    default:
+      return page;
+  }
+}
+
+export function nextPageAction(step = 1) {
+  return {
+    type: "next",
+    step,
+  };
+}
+
+export function previousPageAction(step = 1) {
+  return {
+    type: "previous",
+    step,
+  };
+}
 
 export default function usePagination(initPage) {
-  const [page, setPage] = useState(initPage);
+  const [page, dispatchPagination] = useReducer(paginationReducer, initPage);
 
-  function onNext() {
-    setPage((page) => page + 1);
-  }
-
-  function onPrevious() {
-    setPage((page) => page - 1);
-  }
-
-  return {
-    page,
-    onNext,
-    onPrevious,
-  };
+  return { page, dispatchPagination };
 }
