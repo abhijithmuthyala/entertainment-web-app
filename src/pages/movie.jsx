76c5fa09b896b1768bb8f6d31cb5d8125e8de3b1
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2024-07-05 12:02:54 +0530

    Setup pagination for /movies

--- src/pages/movie.jsx
+++ src/pages/movie.jsx
@@ -4,7 +4,12 @@ import MediaSectionGrid from "@/components/media/MediaSectionGrid";
 
 import { API } from "@/constants";
 import { insertMediaTypeField } from "@/helpers";
+import usePagination, {
+  nextPageAction,
+  previousPageAction,
+} from "@/hooks/usePagination";
 import { fetchData } from "@/utils";
+import { useState } from "react";
 
 export default function MoviesPage({ moviesData }) {
   return (
@@ -20,27 +25,67 @@ export default function MoviesPage({ moviesData }) {
         <h1 className="sr-only">
           Explore a wide collection of movies across genres
         </h1>
-        <MediaSectionGrid heading="movies" mediaData={moviesData} />
+        <PaginatedMovies initMovies={moviesData} />
+        {/* <MediaSectionGrid heading="movies" mediaData={moviesData} /> */}
       </main>
+    </>
+  );
+}
+
+function PaginatedMovies({ initMovies }) {
+  const { page, dispatchPagination } = usePagination(1);
+  const [movies, setMovies] = useState(initMovies);
+
+  async function handleNext() {
+    dispatchPagination(nextPageAction(1));
+    const movies = await fetchMovies(page + 1);
+    setMovies(movies);
+  }
+
+  async function handlePrevious() {
+    dispatchPagination(previousPageAction(1));
+    const movies = await fetchMovies(page - 1);
+    setMovies(movies);
+  }
+
+  return (
+    <>
+      <MediaSectionGrid heading="movies" mediaData={movies} />
+      <div className="flex items-center gap-4">
+        <button onClick={handlePrevious}>Previous</button>
+        Page {page} of 10
+        <button onClick={handleNext}>Next</button>
+      </div>
     </>
   );
 }
 
 export async function getStaticProps() {
   try {
-    const movies = await fetchData(API.movies);
+    const movies = await fetchData(API.movies(1));
     insertMediaTypeField(movies.results, "movie");
 
     return {
       props: {
         moviesData: movies.results,
       },
-      revalidate: 60 * 60 * 24,
+      revalidate: 24 * 60 * 60,
     };
   } catch (error) {
     console.error(error.message);
     return {
       notFound: true,
     };
+  }
+}
+
+async function fetchMovies(page = 1) {
+  try {
+    const movies = await fetchData(`/api/movies?page=${page}`);
+    insertMediaTypeField(movies, "movie");
+    return movies;
+  } catch (error) {
+    console.error(error.message);
+    throw error;
   }
 }
